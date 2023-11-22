d750bed254af5974909a5dcc271d4ee2024744ac
Ali Cataltepe <ali@cataltepe.com>
2023-11-21 19:11:08 -0800

    fixed indentation in control/model_utils.py

--- software/control/model_utils.py
+++ software/control/model_utils.py
@@ -11,31 +11,31 @@ import sys
 
 # runs model
 def run_model(model, device, images, batch_size_inference=2048):
-	
-	predictions = generate_predictions(model,device,images,batch_size_inference)
-	return predictions
+
+    predictions = generate_predictions(model,device,images,batch_size_inference)
+    return predictions
 
 def generate_predictions(model, device, images, batch_size_inference = 2048):
 
-	if images.dtype == np.uint8:
-		images = images.astype(np.float32)/255.0 # convert to 0-1 if uint8 input
+    if images.dtype == np.uint8:
+        images = images.astype(np.float32)/255.0 # convert to 0-1 if uint8 input
 
-	# build dataset
-	dataset = TensorDataset(torch.from_numpy(images), torch.from_numpy(np.ones(images.shape[0])))
+    # build dataset
+    dataset = TensorDataset(torch.from_numpy(images), torch.from_numpy(np.ones(images.shape[0])))
 
-	# dataloader
-	dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size_inference, shuffle=False)
+    # dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size_inference, shuffle=False)
 
-	# run inference 
-	all_predictions = []
-	t0 = time.time()
-	for k, (images, labels) in enumerate(dataloader):
-		images = images.float().to(device)
-		predictions, features = model.get_predictions_and_features(images)
-		predictions = predictions.detach().cpu().numpy()
-		all_predictions.append(predictions)
-	
-	predictions = np.vstack(all_predictions)
-	print('running inference on ' + str(predictions.shape[0]) + ' images took ' + str(time.time()-t0) + ' s')
+    # run inference 
+    all_predictions = []
+    t0 = time.time()
+    for k, (images, labels) in enumerate(dataloader):
+        images = images.float().to(device)
+        predictions, features = model.get_predictions_and_features(images)
+        predictions = predictions.detach().cpu().numpy()
+        all_predictions.append(predictions)
 
-	return predictions
+    predictions = np.vstack(all_predictions)
+    print('running inference on ' + str(predictions.shape[0]) + ' images took ' + str(time.time()-t0) + ' s')
+
+    return predictions
