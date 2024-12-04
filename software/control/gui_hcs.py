a0c6ef55b42f93b6a0371a3ba15cfac699dd0bd6
Soham Mukherjee <soham1202@berkeley.edu>
2024-12-03 22:55:57 -0800

    cleanup

--- software/control/gui_hcs.py
+++ software/control/gui_hcs.py
@@ -664,7 +664,8 @@ class HighContentScreeningGui(QMainWindow):
         self.objectivesWidget.signal_objective_changed.connect(self.navigationViewer.on_objective_changed)
         if ENABLE_FLEXIBLE_MULTIPOINT:
             self.objectivesWidget.signal_objective_changed.connect(self.flexibleMultiPointWidget.update_fov_positions)
-        self.navigationController.xyPos.connect(self.navigationViewer.update_current_location)
+        self.navigationController.xyPos.connect(self.navigationViewer.draw_fov_current_location)
+        self.navigationController.scanGridPos.connect(self.navigationViewer.draw_scan_grid)
         self.multipointController.signal_register_current_fov.connect(self.navigationViewer.register_fov)
         self.multipointController.signal_current_configuration.connect(self.liveControlWidget.set_microscope_mode)
         self.multipointController.signal_z_piezo_um.connect(self.piezoWidget.update_displacement_um_display)
@@ -705,7 +706,7 @@ class HighContentScreeningGui(QMainWindow):
         if ENABLE_WELLPLATE_MULTIPOINT:
             self.wellSelectionWidget.signal_wellSelected.connect(self.wellplateMultiPointWidget.set_well_coordinates)
             self.objectivesWidget.signal_objective_changed.connect(self.wellplateMultiPointWidget.update_coordinates)
-            self.wellplateMultiPointWidget.signal_update_navigation_viewer.connect(self.navigationViewer.update_current_location)
+            self.wellplateMultiPointWidget.signal_update_navigation_viewer.connect(self.navigationViewer.draw_fov_current_location)
 
         if SUPPORT_LASER_AUTOFOCUS:
             self.liveControlWidget_focus_camera.signal_newExposureTime.connect(self.cameraSettingWidget_focus_camera.set_exposure_time)
