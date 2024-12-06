5dab57c172ef7c29e765f01a35140123981824e2
Soham Mukherjee <soham1202@berkeley.edu>
2024-12-06 01:50:57 -0800

    delay scan grid generation

--- software/control/core.py
+++ software/control/core.py
@@ -726,6 +726,7 @@ class NavigationController(QObject):
     zPos = Signal(float)
     thetaPos = Signal(float)
     xyPos = Signal(float,float)
+    scanGridPos = Signal(float,float)
     signal_joystick_button_pressed = Signal()
 
     # x y z axis pid enable flag
@@ -754,10 +755,11 @@ class NavigationController(QObject):
         # to be moved to gui for transparency
         self.microcontroller.set_callback(self.update_pos)
 
-        # self.timer_read_pos = QTimer()
-        # self.timer_read_pos.setInterval(PosUpdate.INTERVAL_MS)
-        # self.timer_read_pos.timeout.connect(self.update_pos)
-        # self.timer_read_pos.start()
+        self.movement_timer = QTimer(self)
+        self.movement_timer.setInterval(500)  # Single 500ms check
+        self.movement_timer.timeout.connect(self.check_movement_status)
+        self.movement_timer.setSingleShot(True)  # Timer only fires once
+        self.movement_threshold = 0.0001  # mm
 
     def get_mm_per_ustep_X(self):
         return SCREW_PITCH_X_MM/(self.x_microstepping*FULLSTEPS_PER_REV_X)
@@ -773,6 +775,104 @@ class NavigationController(QObject):
 
     def get_flag_click_to_move(self):
         return self.click_to_move
+
+    def update_pos(self,microcontroller):
+        # get position from the microcontroller
+        last_x_pos = self.x_pos_mm
+        last_y_pos = self.y_pos_mm
+        x_pos, y_pos, z_pos, theta_pos = microcontroller.get_pos()
+        self.z_pos = z_pos
+        # calculate position in mm or rad
+        if USE_ENCODER_X:
+            self.x_pos_mm = x_pos*ENCODER_POS_SIGN_X*ENCODER_STEP_SIZE_X_MM
+        else:
+            self.x_pos_mm = x_pos*STAGE_POS_SIGN_X*self.get_mm_per_ustep_X()
+        if USE_ENCODER_Y:
+            self.y_pos_mm = y_pos*ENCODER_POS_SIGN_Y*ENCODER_STEP_SIZE_Y_MM
+        else:
+            self.y_pos_mm = y_pos*STAGE_POS_SIGN_Y*self.get_mm_per_ustep_Y()
+        if USE_ENCODER_Z:
+            self.z_pos_mm = z_pos*ENCODER_POS_SIGN_Z*ENCODER_STEP_SIZE_Z_MM
+        else:
+            self.z_pos_mm = z_pos*STAGE_POS_SIGN_Z*self.get_mm_per_ustep_Z()
+        if USE_ENCODER_THETA:
+            self.theta_pos_rad = theta_pos*ENCODER_POS_SIGN_THETA*ENCODER_STEP_SIZE_THETA
+        else:
+            self.theta_pos_rad = theta_pos*STAGE_POS_SIGN_THETA*(2*math.pi/(self.theta_microstepping*FULLSTEPS_PER_REV_THETA))
+
+        # emit the updated position
+        self.xPos.emit(self.x_pos_mm)
+        self.yPos.emit(self.y_pos_mm)
+        self.zPos.emit(self.z_pos_mm*1000)
+        self.thetaPos.emit(self.theta_pos_rad*360/(2*math.pi))
+        self.xyPos.emit(self.x_pos_mm,self.y_pos_mm)
+
+        if microcontroller.signal_joystick_button_pressed_event:
+            if self.enable_joystick_button_action:
+                self.signal_joystick_button_pressed.emit()
+            print('joystick button pressed')
+            microcontroller.signal_joystick_button_pressed_event = False
+
+        # Check if position has changed
+        if last_x_pos != self.x_pos_mm or last_y_pos != self.y_pos_mm:
+            # restart movement timer
+            QMetaObject.invokeMethod(self.movement_timer, "start", Qt.QueuedConnection)
+
+    def check_movement_status(self):
+        """Check if stage has stopped moving after timer delay"""
+        x_pos, y_pos, z_pos, theta_pos = self.microcontroller.get_pos()
+        # calculate position in mm or rad
+        if USE_ENCODER_X:
+            x_pos_mm = x_pos*ENCODER_POS_SIGN_X*ENCODER_STEP_SIZE_X_MM
+        else:
+            x_pos_mm = x_pos*STAGE_POS_SIGN_X*self.get_mm_per_ustep_X()
+        if USE_ENCODER_Y:
+            y_pos_mm = y_pos*ENCODER_POS_SIGN_Y*ENCODER_STEP_SIZE_Y_MM
+        else:
+            y_pos_mm = y_pos*STAGE_POS_SIGN_Y*self.get_mm_per_ustep_Y()
+
+        delta_x = abs(self.x_pos_mm - x_pos_mm)
+        delta_y = abs(self.y_pos_mm - y_pos_mm)
+
+        # check if movement less than thresshold (i.e. stopped moving)
+        if delta_x < self.movement_threshold and delta_y < self.movement_threshold and not self.microcontroller.is_busy():
+            # emit pos to draw scan grid
+            self.scanGridPos.emit(self.x_pos_mm, self.y_pos_mm)
+
+    def scan_preview_move_from_click(self, click_x, click_y, image_width, image_height, Nx=1, Ny=1, dx_mm=0.9, dy_mm=0.9): # obsolete (only for tiled display)
+        """
+        napariTiledDisplay uses the Nx, Ny, dx_mm, dy_mm fields to move to the correct fov first
+        imageArrayDisplayWindow assumes only a single fov (default values do not impact calculation but this is less correct)
+        """
+        # check if click to move enabled
+        if not self.click_to_move:
+            print("allow click to move")
+            return
+        # restore to raw coordicate
+        click_x = image_width / 2.0 + click_x
+        click_y = image_height / 2.0 - click_y
+        print("click - (x, y):", (click_x, click_y))
+        fov_col = click_x * Nx // image_width
+        fov_row = click_y * Ny // image_height
+        end_position_x = Ny % 2 # right side or left side
+        fov_col = Nx - (fov_col + 1) if end_position_x else fov_col
+        fov_row = fov_row
+        pixel_sign_x = (-1)**end_position_x # inverted
+        pixel_sign_y = -1 if INVERTED_OBJECTIVE else 1
+
+        # move to selected fov
+        x_pos = self.scan_begin_position_x+dx_mm*fov_col*pixel_sign_x
+        y_pos = self.scan_begin_position_y+dy_mm*fov_row*pixel_sign_y
+        self.move_to(x_pos, y_pos)
+
+        # move to actual click, offset from center fov
+        tile_width = (image_width / Nx) * PRVIEW_DOWNSAMPLE_FACTOR
+        tile_height = (image_height / Ny) * PRVIEW_DOWNSAMPLE_FACTOR
+        offset_x = (click_x * PRVIEW_DOWNSAMPLE_FACTOR) % tile_width
+        offset_y = (click_y * PRVIEW_DOWNSAMPLE_FACTOR) % tile_height
+        offset_x_centered = int(offset_x - tile_width / 2)
+        offset_y_centered = int(tile_height / 2 - offset_y)
+        self.move_from_click(offset_x_centered, offset_y_centered, tile_width, tile_height)
 
     def move_from_click(self, click_x, click_y, image_width, image_height):
         if self.click_to_move:
@@ -852,40 +952,6 @@ class NavigationController(QObject):
 
     def move_z_to_usteps(self,usteps):
         self.microcontroller.move_z_to_usteps(usteps)
-
-    def update_pos(self,microcontroller):
-        # get position from the microcontroller
-        x_pos, y_pos, z_pos, theta_pos = microcontroller.get_pos()
-        self.z_pos = z_pos
-        # calculate position in mm or rad
-        if USE_ENCODER_X:
-            self.x_pos_mm = x_pos*ENCODER_POS_SIGN_X*ENCODER_STEP_SIZE_X_MM
-        else:
-            self.x_pos_mm = x_pos*STAGE_POS_SIGN_X*self.get_mm_per_ustep_X()
-        if USE_ENCODER_Y:
-            self.y_pos_mm = y_pos*ENCODER_POS_SIGN_Y*ENCODER_STEP_SIZE_Y_MM
-        else:
-            self.y_pos_mm = y_pos*STAGE_POS_SIGN_Y*self.get_mm_per_ustep_Y()
-        if USE_ENCODER_Z:
-            self.z_pos_mm = z_pos*ENCODER_POS_SIGN_Z*ENCODER_STEP_SIZE_Z_MM
-        else:
-            self.z_pos_mm = z_pos*STAGE_POS_SIGN_Z*self.get_mm_per_ustep_Z()
-        if USE_ENCODER_THETA:
-            self.theta_pos_rad = theta_pos*ENCODER_POS_SIGN_THETA*ENCODER_STEP_SIZE_THETA
-        else:
-            self.theta_pos_rad = theta_pos*STAGE_POS_SIGN_THETA*(2*math.pi/(self.theta_microstepping*FULLSTEPS_PER_REV_THETA))
-        # emit the updated position
-        self.xPos.emit(self.x_pos_mm)
-        self.yPos.emit(self.y_pos_mm)
-        self.zPos.emit(self.z_pos_mm*1000)
-        self.thetaPos.emit(self.theta_pos_rad*360/(2*math.pi))
-        self.xyPos.emit(self.x_pos_mm,self.y_pos_mm)
-
-        if microcontroller.signal_joystick_button_pressed_event:
-            if self.enable_joystick_button_action:
-                self.signal_joystick_button_pressed.emit()
-            print('joystick button pressed')
-            microcontroller.signal_joystick_button_pressed_event = False
 
     def home_x(self):
         self.microcontroller.home_x()
@@ -3605,6 +3671,10 @@ class NavigationViewer(QFrame):
         self.view.addItem(self.scan_overlay_item)
         self.view.addItem(self.fov_overlay_item)
 
+        self.background_item.setZValue(-1)  # Background layer at the bottom
+        self.scan_overlay_item.setZValue(0)  # Scan overlay in the middle
+        self.fov_overlay_item.setZValue(1)  # FOV overlay on top
+
     def update_display_properties(self, sample):
         if sample == 'glass slide':
             self.location_update_threshold_mm = 0.2
@@ -3682,29 +3752,19 @@ class NavigationViewer(QFrame):
         self.update_display_properties(sample)
         self.draw_current_fov(self.x_mm, self.y_mm)
 
-    def update_current_location(self, x_mm=None, y_mm=None):
+    def draw_fov_current_location(self, x_mm=None, y_mm=None):
         if x_mm is None and y_mm is None:
             if self.x_mm is None and self.y_mm is None:
                 return
-            else:
-                self.draw_current_fov(self.x_mm, self.y_mm)
-
-        elif self.x_mm is not None and self.y_mm is not None:
-            # update only when the displacement has exceeded certain value
-            if abs(x_mm - self.x_mm) > self.location_update_threshold_mm or abs(y_mm - self.y_mm) > self.location_update_threshold_mm:
-                self.draw_current_fov(x_mm, y_mm)
-                self.x_mm = x_mm
-                self.y_mm = y_mm
-                # update_live_scan_grid
-                if 'glass slide' in self.sample and not self.acquisition_started:
-                    self.signal_update_live_scan_grid.emit(x_mm, y_mm)
+            self.draw_current_fov(self.x_mm, self.y_mm)
         else:
             self.draw_current_fov(x_mm, y_mm)
             self.x_mm = x_mm
             self.y_mm = y_mm
-            # update_live_scan_grid
-            if 'glass slide' in self.sample and not self.acquisition_started:
-                self.signal_update_live_scan_grid.emit(x_mm, y_mm)
+
+    def draw_scan_grid(self, x_mm, y_mm):
+        if 'glass slide' in self.sample and not self.acquisition_started:
+            self.signal_update_live_scan_grid.emit(x_mm, y_mm)
 
     def get_FOV_pixel_coordinates(self, x_mm, y_mm):
         if self.sample == 'glass slide':
